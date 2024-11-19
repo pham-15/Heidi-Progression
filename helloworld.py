59d15a7e1fcc5244561100585262a350f1a9fcf2
Eunbi007 <HeidiEunbiLee@gmail.com>
2024-11-19 00:29:56 -0600

    while loop

--- helloworld.py
+++ helloworld.py
@@ -1,74 +1,71 @@
 msg = "\n\nQuiz: What Type of Fruit Are You?\n"
 print (msg)
 
-#points 
-points = 0
-
 #Questions 
 
-Q1 = input("Sweet or Sour? \nA) Sweet \nB) Sour\n")
-if Q1 == "A":
-    points += 1
-elif Q1 == "B":
-    points += 2
-else: 
-    print("The answer you put in is not an answer choice please try again.\n")
-    Q1 = input("Sweet or Sour? \nA) Sweet \nB) Sour\n")
-    if Q1 == "A":
-        points += 1
-    elif Q1 == "B":
-        points += 2
+repeat = ""
+while repeat != "no":
+    repeat = input("Would you like to play this game? \nyes\nno\n")
+    points = 0
+    if repeat == "yes":
+        Q1 = ""
+        while Q1 != "A" or Q1 != "B":
+            Q1 = input("\nSweet or Sour? \nA) Sweet \nB) Sour\n")
+            if Q1 == "A":
+                points += 1
+                break 
+            elif Q1 == "B":
+                points += 2 
+                break 
 
-Q2 = input("Tropical getaway or Countryside? \nA) Tropical getaway \nB) Countryside\n")
-if Q2 == "A":
-    points += 1
-elif Q2 == "B":
-    points += 2
-else: 
-    print("The answer you put in is not an answer choice please try again.\n")
-    Q2 = input("Tropical getaway or Countryside? \nA) Tropical getaway \nB) Countryside\n")
-    if Q2 == "A":
-        points += 1
-    elif Q2 == "B":
-        points += 2
+        Q2 = ""
+        while Q2 != "A" or Q2 != "B":
+            Q2 = input("Tropical getaway or Countryside? \nA) Tropical getaway \nB) Countryside\n")
+            if Q2 == "A":
+                points += 1
+                break 
+            elif Q2 == "B":
+                points += 2 
+                break 
 
-Q3 = input("What color would you choose? \nA) Red \nB) Yellow \nC) Green \nD) Blue\n")
-if Q3 == "A":
-    points += 1
-elif Q3 == "B":
-    points += 2
-elif Q3 == "C":
-    points += 3
-elif Q3 == "D":
-    points += 4
-else:
-    print("The answer you put in is not an answer choice please try again.\n")
-    Q3 = input("What color would you choose? \nA) Red \nB) Yellow \nC) Green \nD) Blue\n")
-    if Q3 == "A":
-        points += 1
-    elif Q3 == "B":
-        points += 2
-    elif Q3 == "C":
-        points += 3
-    elif Q3 == "D":
-        points += 4
+        Q3 = ""
+        while Q3 != "A" or Q3 != "B" or Q3 != "C" or Q3 != "D":
+            Q3 = input("What color would you choose? \nA) Red \nB) Yellow \nC) Green \nD) Blue\n")
+            if Q3 == "A":
+                points += 1
+                break
+            elif Q3 == "B":
+                points += 2
+                break
+            elif Q3 == "C":
+                points += 3
+                break
+            elif Q3 == "D":
+                points += 4
+                break
 
 
-print ("\n\nThe total point: " + str(points) + "\n")
+        print ("\n\nThe total point: " + str(points) + "\n")
+        
 
-fruit = ""
-if points == 3:
-    fruit = "Apple"
-elif points == 4:
-    fruit = "Papaya"
-elif points == 5:
-    fruit = "Grapefruit"
-elif points == 6:
-    fruit = "Orange"
-elif points == 7:
-    fruit = "Apple"
-elif points == 8:
-    fruit = "Grape"
-else:
-    fruit = "Blueberry"
-print ("Your fruit is a(n) " + fruit)
+        fruit = ""
+        if points == 3:
+            fruit = "Apple"
+        elif points == 4:
+            fruit = "Papaya"
+        elif points == 5:
+            fruit = "Grapefruit"
+        elif points == 6:
+            fruit = "Orange"
+        elif points == 7:
+            fruit = "Pineapple"
+        elif points == 8:
+            fruit = "Grape"
+        else:
+            fruit = "Blueberry"
+        print ("Your fruit is a(n) " + fruit + "\n\n")
+        points = 0; 
+    
+    elif repeat == "no":
+        print ("\nThank you for playing.\n")
+        break
